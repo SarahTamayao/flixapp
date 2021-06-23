f554ae1add438bb9ffe00db3162393e32764730d
Laura Yao <ljyao@fb.com>
2021-06-23 13:27:41 -0400

    table view

---
+++ flixapp/View Controllers/MoviesViewController.h
@@ -0,0 +1,16 @@
+//
+//  MoviesViewController.h
+//  flixapp
+//
+//  Created by Laura Yao on 6/23/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MoviesViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
